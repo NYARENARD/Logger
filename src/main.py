ebb46174db9b479179e6df6dc3f20253a002cc27
Vitaliy Kovalev <kovalevvyu@student.bmstu.ru>
2022-10-05 21:51:08 +0300

    tyhh

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from threading import Thread
 from config_vars import class_vars
 from logger_class import Logger
-import typespam
 
 def main():
     instance = Logger(class_vars)
     instance.start()
-    t = Thread(target=typespam.main, args=())
-    t.start()
-    
 
 main()
